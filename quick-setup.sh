df4e3790eb916d2bc310c0b2b18f879a6ff58e71
yong <ycao181@gmail.com>
2022-03-01 21:40:50 -0800

    minor update

--- quick-setup.sh
+++ quick-setup.sh
@@ -6,7 +6,7 @@
 #
 
 apt-get update
-apt-get install -y zsh curl git
+apt-get install -y zsh curl git vim
 
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -15,5 +15,4 @@ mkdir ~/.zsh
 rm -rf /tmp/cloud
 git clone https://github.com/gfxcc/cloud.git /tmp/cloud
 
-copy /tmp/cloud/dot_files/* ~
-# 
+cp -r /tmp/cloud/dot_files/. ~
