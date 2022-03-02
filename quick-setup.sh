319b575e74011a543abcb807779075c1e1421854
yong <ycao181@gmail.com>
2022-03-01 21:52:36 -0800

    minor update

--- quick-setup.sh
+++ quick-setup.sh
@@ -6,7 +6,7 @@
 #
 
 apt-get update
-apt-get install -y zsh curl git vim
+apt-get install -y zsh curl git vim tmux
 
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" 0</dev/null
