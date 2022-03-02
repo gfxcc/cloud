34f770fd785d1687469a45ca2950d98be43baa37
yong <ycao181@gmail.com>
2022-03-01 22:02:08 -0800

    minor update

--- quick-setup.sh
+++ quick-setup.sh
@@ -5,8 +5,8 @@
 # Config Zsh, tmux
 #
 
-apt-get update
-apt-get install -y zsh curl git vim tmux
+sudo apt-get update
+sudo apt-get install -y zsh curl git vim tmux
 
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" 0</dev/null
